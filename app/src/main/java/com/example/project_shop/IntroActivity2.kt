9e2af803ffa4656e7b44fa5cc6db936964f1bd26
ALVINA <1473967@senati.pe>
2024-10-02 11:41:31 -0500

    actualizando

--- app/src/main/java/com/example/project_shop/IntroActivity2.kt
+++ app/src/main/java/com/example/project_shop/IntroActivity2.kt
@@ -1,18 +1,32 @@
 package com.example.project_shop
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Html
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.LayoutInflaterCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.project_shop.databinding.ActivityIntro2Binding
 
 class IntroActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntro2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_intro2)
+        // Inflar el binding correctamente utilizando 'layoutInflater'
+        binding = ActivityIntro2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            startBtn.setOnClickListener {
+                startActivity(Intent(this@IntroActivity2, MainActivity::class.java))
+            }
+        }
 
         // Aplicar padding para las barras del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
