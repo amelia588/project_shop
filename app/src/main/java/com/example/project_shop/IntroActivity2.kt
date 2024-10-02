c4862bf2c67f6be263f42fb9094183232cc1309f
ALVINA <1473967@senati.pe>
2024-10-02 03:43:15 -0500

    mi pantalla principal

--- app/src/main/java/com/example/project_shop/IntroActivity2.kt
+++ app/src/main/java/com/example/project_shop/IntroActivity2.kt
@@ -1,6 +1,8 @@
 package com.example.project_shop
 
 import android.os.Bundle
+import android.text.Html
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,20 @@ class IntroActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_intro2)
+
+        // Aplicar padding para las barras del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Manejo del texto con formato HTML
+        val textView: TextView = findViewById(R.id.textView3)
+        val formattedText = Html.fromHtml(
+            "Welcome to <font color='#39c7a5'>Digital Store</font><br> a <font color='#39c7a5'>Gateway</font> to the Latest Tech",
+            Html.FROM_HTML_MODE_LEGACY
+        )
+        textView.text = formattedText
     }
 }
